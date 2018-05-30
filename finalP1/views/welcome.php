dbc9638b4ce5b9a1c5752d12a951e1675be1dccb
Diana Carranza <dcarranza015@gmail.com>
2018-05-29 23:21:18 -0700

    Welcome page general layout and form is complete

--- finalP1/views/welcome.php
+++ finalP1/views/welcome.php
@@ -9,8 +9,46 @@
 <include href="views/parts/header.php"></include>
 
 <div class="container">
-    <h3> Work</h3>
+    <div class="row">
+        <div class="col-sm-4" style="background-color:cadetblue;">
+            <div class="form-group">
+                <h2>Login to Navigate</h2>
+            </div>
+        </div>
+        <div class="col-sm" style="background-color:dodgerblue;">
+            <div class="form-group row">
+                <div class="col-sm">
+                    <h1 align="center">Welcome to Bookmark Share</h1>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm">
+                    <h3 align="center"> Please login or register for a new account.</h3>
+                </div>
+            </div>
 
+            <form>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-8">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-8">
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div align="center" class="col-sm">
+                        <button type="button" class="btn btn-secondary">Login</button>
+                        <button type="button" class="btn btn-secondary">Register</button>
+                    </div>
+                </div>
+
+            </form>
+        </div>
 </div>
 
 

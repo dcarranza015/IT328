39659a1cd274802f9a7ff9aa4e9914efb0e7f81a
Diana Carranza <dcarranza015@gmail.com>
2018-06-07 19:13:07 -0700

    Fixed the connection to the DB and verified that it is accessible

--- Final/model/bookmarkDB.php
+++ Final/model/bookmarkDB.php
@@ -29,3 +29,154 @@ function getConnection()
 
     return $connection;
 }
+
+function checkUser($email, $password)
+{
+    $q = "SELECT * FROM users WHERE 'email' = '$email' AND 'password' = '$password'";
+
+    $rq = mysqli_query(getConnection(), $q);
+
+    //check was the query successful
+    if ($rq)
+    {
+        var_dump($rq);
+       // return $r = mysqli_fetch_assoc($rq);
+    }
+    else return false;
+
+}
+
+
+/**
+ * returns all books stored in the database
+ * @param $connection
+ * @return array
+ */
+//returns all books stored in the database
+function getBooks($connection)
+{
+    //query for message records
+    $query = 'SELECT * FROM books';
+
+    $results = mysqli_query($connection, $query);
+
+    if (!$results)
+    {
+        echo 'Error retrieving records';
+        exit;
+    }
+
+    $books = array();
+    while ($row = mysqli_fetch_assoc($results))
+    {
+        $books[] = $row;
+    }
+
+    //frees results
+    mysqli_free_result($results);
+
+    return $books;
+}
+
+/**
+ * retrieves book
+ * @param $id
+ * @return array|bool|null
+ */
+function getBook($id)
+{
+
+
+    $q = "SELECT * FROM books WHERE id = '$id'";
+
+    $rq = mysqli_query(getConnection(), $q);
+
+    //check was the query successful
+    if ($rq)
+    {
+        return $r = mysqli_fetch_assoc($rq);
+    }
+    else return false;
+
+
+}
+
+/**
+ * adds a new book to the database
+ * @param $connection
+ * @param $title
+ * @param $fiction
+ * @param $publisher
+ * @param $summary
+ * @param $pages
+ * @return bool
+ */
+function addBook($title, $fiction, $publisher, $summary, $pages)
+{
+    $q = "INSERT INTO books (title, fiction, publisher, summary, pages) VALUES ('$title', '$fiction', '$publisher', '$summary', '$pages')";
+
+    $r = mysqli_query(getConnection(), $q); // Run the query.
+
+    //check was the query successful
+    if ($r)
+    {
+        return true;
+    }
+    else return false;
+}
+
+/**
+ * searches for the book with the given id and updates all attributes with the input parameters
+ * @param $id
+ * @param $title
+ * @param $fiction
+ * @param $publisher
+ * @param $summary
+ * @param $pages
+ * @return bool
+ */
+function updateBook($id, $title, $fiction, $publisher, $summary, $pages)
+{
+
+    $q = "UPDATE books 
+          SET title = '$title', fiction = '$fiction', publisher = '$publisher', summary = '$summary', pages = '$pages' 
+          WHERE id = '$id' ";
+
+    $r = mysqli_query(getConnection(), $q); // Run the query.
+
+    //check was the query successful
+    if ($r)
+    {
+        return true;
+    }
+    else return false;
+
+
+}
+
+/**
+ * searches for the book with the given id and removes it from the database
+ * @param $connection
+ */
+
+function deleteBook($connection)
+{
+
+    //get all the information from the DB
+    $id = $_GET['id'];
+
+    $q = "SELECT * FROM books WHERE id = '$id'";
+
+    $rq = mysqli_query(getConnection(), $q);
+
+    $_SESSION = mysqli_fetch_assoc($rq);
+
+    $deleteQuery = "DELETE FROM books  WHERE id = '$id' ";
+
+    if (mysqli_query($connection, $deleteQuery))
+    {
+        //delete successful (true)
+    }
+    else false;
+
+}
